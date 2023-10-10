8fef0b3ce770a3e305881b19a9fce8d423126c4f
LemonjamesD <lemon@lemonjamesd.com>
2023-10-10 18:40:53 -0400

    Implementing basic functions

--- src/Starry.hs
+++ src/Starry.hs
@@ -1,15 +1,48 @@
-module Starry (World) where
+{-# LANGUAGE RecordWildCards #-}
+module Starry (World, newWorld, addComponent, addResource, addSystem) where
 
-import Data.Typeable (TypeRep)
-import Data.HashMap.Strict (HashMap)
+import Data.Typeable (TypeRep, typeOf, Typeable)
+import Data.HashMap.Strict (HashMap, empty, insert)
 import Starry.Components (Component)
 import Starry.Resources (Resource)
 
 data Dynamic c where 
   Dynamic :: c a => a -> Dynamic c
 
+type SystemType = (World -> IO World)
+
 data World = World {
   components :: [Dynamic Component]
+, takenIndices :: [Int]
 , resources :: HashMap (TypeRep) (Dynamic Resource)
-, systems :: [(World -> World)]
+, systems :: [SystemType]
 }
+
+newWorld :: World
+newWorld = World {
+  components = []
+, takenIndices = []
+, resources = empty
+, systems = []
+}
+
+addComponent :: Component a => a -> World -> World
+addComponent comp world@(World { .. })= world {
+  components = components ++ [Dynamic comp]
+}
+
+addResource :: (Resource a, Typeable a) => a -> World -> World
+addResource res world@(World { .. }) = world {
+  resources = insert (typeOf res) (Dynamic res) resources
+}
+
+addSystem :: SystemType -> World -> World
+addSystem system world@(World { .. }) = world {
+  systems = systems ++ [system]
+}
+
+-- getComponents :: Component a => World -> (World, [a])
+-- getComponents world@(World { .. }) = 
+
+-- updateComponents :: Component a => [a] -> World
+-- updateComponents comps world@(World { .. }) = world 
