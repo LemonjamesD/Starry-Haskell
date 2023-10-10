a5dcb5fd00a0afee38e5bba88714589465fcf566
LemonjamesD <lemon@lemonjamesd.com>
2023-10-10 14:18:45 -0400

    Added all the types

--- src/Starry.hs
+++ src/Starry.hs
@@ -2,12 +2,13 @@ module Starry (World) where
 
 import Data.HashMap.Strict (HashMap)
 import Starry.Components (Component)
+import Starry.Resources (Resource)
 
 data Dynamic c where 
   Dynamic :: c a => a -> Dynamic c
 
 data World = World {
   components :: [Dynamic Component]
-, resources :: HashMap () ()
-, systems :: [()]
+, resources :: HashMap (TypeRep) (Dynamic Resource)
+, systems :: [(World -> World)]
 }
